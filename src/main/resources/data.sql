c4fb6fbc8fd27ad6da32691ccb7e27d717b67163
LucasSpenthof <lucas.spenthof@gmail.com>
2021-11-19 14:44:59 -0400

    Correção do insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_servidor(nome, telefone, email, senha)
+    VALUES('Pedro Cavalcanti', '(67)99999-8888', 'pedro@gmail.com', '123456');
+
+INSERT INTO tb_servidor(nome, telefone, email, senha)
+    VALUES('Maria Izabel', '(67)88888-7777', 'maria@gmail.com', '654321');
+
